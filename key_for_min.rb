6b209cb9303c24777b9b09c090a5d6070b97870c
Gabriel Hicks <gabrielhicks@mail.com>
2020-08-19 16:50:38 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -23,3 +23,20 @@ require 'pry'
 #   min_key
 # end
 
+def key_for_min_value(name_hash)
+  lowest_value = 10000
+  
+  name_hash.each do |key, value|
+    if value < lowest_value
+      lowest_value = value
+    end
+  end
+  
+  name_hash.each do |key, value|
+    if lowest_value == value
+      p key
+      binding.pry
+    end
+  end
+  
+end
